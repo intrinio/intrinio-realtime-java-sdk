89f24cf4e32ecabe5af2e68022f6df12ece22d50
Evan <ehyde@intrinio.com>
2023-05-09 17:23:06 -0600

    version bump

--- src/intrinio/Client.java
+++ src/intrinio/Client.java
@@ -570,7 +570,7 @@ public class Client implements WebSocket.Listener {
 		HttpURLConnection con;
 		try {
 			con = (HttpURLConnection) url.openConnection();
-			con.setRequestProperty("Client-Information", "IntrinioRealtimeJavaSDKv5.0");
+			con.setRequestProperty("Client-Information", "IntrinioRealtimeJavaSDKv6.0");
 		} catch (IOException e) {
 			Client.Log("Authorization Failure. Please check your network connection. " + e.getMessage());
 			return false;

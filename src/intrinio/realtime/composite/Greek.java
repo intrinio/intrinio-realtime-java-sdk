cd8b4c0db1c41fceadd7c0f6e6f6b0b571cb9252
Shawn Snyder <ssnyder@intrinio.com>
2024-05-07 01:07:27 -0500

    Add attributes we'll need to fill in.

--- src/intrinio/realtime/composite/Greek.java
+++ src/intrinio/realtime/composite/Greek.java
@@ -1,4 +1,19 @@
 package intrinio.realtime.composite;
 
-public class Greek {
+public record Greek (String ticker,
+                     String contract,
+                     intrinio.realtime.equities.Trade latestEquityTrade,
+                     intrinio.realtime.options.Trade latestOptionTrade,
+                     intrinio.realtime.options.Quote latestOptionQuote,
+                     double riskFreeInterestRate,
+                     double dividendYield,
+                     double daysToExpiration,
+                     double marketPrice,
+                     double impliedVolatility,
+                     double delta,
+                     double gamma,
+                     double theta,
+                     double vega,
+                     GreekCalculationMethod greekCalculationMethod){
+
 }

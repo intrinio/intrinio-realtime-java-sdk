9ee96b0d07ec950615f08e0ecf224df5f1aed5a4
ssnyder-intrinio <87659980+ssnyder-intrinio@users.noreply.github.com>
2024-09-30 12:21:01 -0500

    New exchange mappings and fallback. (#25)

--- src/intrinio/realtime/options/Exchange.java
+++ src/intrinio/realtime/options/Exchange.java
@@ -12,21 +12,27 @@ public enum Exchange {
     ISE((byte)'I'),
     MERCURY((byte)'J'),
     MIAMI((byte)'M'),
+    NYSE_ARCA((byte)'N'),
     MIAMI_PEARL((byte)'O'),
-    NYSE_ARCA((byte)'P'),
     NASDAQ((byte)'Q'),
+    MIAX_SAPPHIRE((byte)'S'),
     NASDAQ_BX((byte)'T'),
     MEMX((byte)'U'),
     CBOE_C2((byte)'W'),
     PHLX((byte)'X'),
-    BATS_BZX((byte)'Z');
+    BATS_BZX((byte)'Z'),
+    UNKNOWN((byte)'?');
     
 	private static final HashMap<Byte,Exchange> exchangeCodeMap = new HashMap<Byte,Exchange>();
 	
 	static {
 		for (Exchange e: values()) {
 			exchangeCodeMap.put(e.code, e);
+            byte lower = (byte)(Character.toLowerCase((char)e.code));
+            exchangeCodeMap.put(lower, e);
 		}
+        exchangeCodeMap.put((byte)'P', Exchange.NYSE_ARCA); //Two chars mapped to same exchange. This one is depreciated
+        exchangeCodeMap.put((byte)'p', Exchange.NYSE_ARCA); //Two chars mapped to same exchange. This one is depreciated
 	}
 	
     public final byte code;
@@ -34,8 +40,9 @@ public enum Exchange {
     	this.code = code;
     }
     
-    public static Exchange valueOfCode(byte code) {
-    	return exchangeCodeMap.get(code);
+    public static Exchange valueOfCode(byte code)
+    {
+        return exchangeCodeMap.getOrDefault(code, Exchange.UNKNOWN);
     }
     
 }

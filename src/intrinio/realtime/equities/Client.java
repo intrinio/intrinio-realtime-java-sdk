3750c280c23d5dac9d4d63a7450bfaa8085d1d4d
Shawn Snyder <ssnyder@intrinio.com>
2026-01-20 09:09:46 -0600

    version bump

--- src/intrinio/realtime/equities/Client.java
+++ src/intrinio/realtime/equities/Client.java
@@ -52,7 +52,7 @@ public class Client implements WebSocket.Listener {
 	private Thread[] processDataThreads;
 	private boolean isCancellationRequested = false;
 	private String HeaderClientInformationKey = "Client-Information";
-	private String HeaderClientInformationValue = "IntrinioRealtimeJavaSDKv8.1";
+	private String HeaderClientInformationValue = "IntrinioRealtimeJavaSDKv8.2";
 	private String HeaderMessageVersionKey = "UseNewEquitiesFormat";
 	private String HeaderMessageVersionValue = "v2";
 	//endregion Data Members
